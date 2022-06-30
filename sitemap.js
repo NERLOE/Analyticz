770e31fc6c2a230ef8c42fe622d6b7e736e5064e
Marcus Nerløe <marcusnerloe@gmail.com>
2022-06-30 16:25:13 +0200

    Sitemap added

--- sitemap.js
+++ sitemap.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://analyticz.marcusnerloe.dk",
   generateRobotsTxt: true, // (optional)
-  exclude: ["/server-sitemap.xml"], // <= exclude here
+  //exclude: ["/server-sitemap.xml"], // <= exclude here
   robotsTxtOptions: {
-    additionalSitemaps: [
+    /*additionalSitemaps: [
       "https://robotbar.dk/server-sitemap.xml", // <==== Add here
-    ],
+    ],*/
   },
 };
